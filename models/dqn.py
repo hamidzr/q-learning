92e8a5cdd0016e2cb375ec5cd687c4d9d624d914
Hamid Zare <dellydela@gmail.com>
2018-04-13 11:51:36 -0500

    changed indentaion from 4 to 2

--- models/dqn.py
+++ models/dqn.py
@@ -7,68 +7,68 @@ from keras.optimizers import Adam
 from keras import backend as K
 
 class DQNAgent:
-    def __init__(self, state_size, action_size, epsilon=1,
-                 epsilon_min=0.01, epsilon_decay=0.99,
-                 discount_rate=0.95, learning_rate=0.001):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = discount_rate    # discount rate
-        self.epsilon = epsilon  # exploration rate
-        self.epsilon_min = epsilon_min
-        self.epsilon_decay = epsilon_decay
-        self.learning_rate = learning_rate
-        self.model = self._build_model()
-        self.target_model = self._build_model()
-        self.update_target_model()
+  def __init__(self, state_size, action_size, epsilon=1,
+         epsilon_min=0.01, epsilon_decay=0.99,
+         discount_rate=0.95, learning_rate=0.001):
+    self.state_size = state_size
+    self.action_size = action_size
+    self.memory = deque(maxlen=2000)
+    self.gamma = discount_rate  # discount rate
+    self.epsilon = epsilon  # exploration rate
+    self.epsilon_min = epsilon_min
+    self.epsilon_decay = epsilon_decay
+    self.learning_rate = learning_rate
+    self.model = self._build_model()
+    self.target_model = self._build_model()
+    self.update_target_model()
 
-    def _huber_loss(self, target, prediction):
-        # sqrt(1+error^2)-1
-        error = prediction - target
-        return K.mean(K.sqrt(1+K.square(error))-1, axis=-1)
+  def _huber_loss(self, target, prediction):
+    # sqrt(1+error^2)-1
+    error = prediction - target
+    return K.mean(K.sqrt(1+K.square(error))-1, axis=-1)
 
-    def _build_model(self):
-        # Neural Net for Deep-Q learning Model
-        model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(24, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss=self._huber_loss,
-                      optimizer=Adam(lr=self.learning_rate))
-        return model
+  def _build_model(self):
+    # Neural Net for Deep-Q learning Model
+    model = Sequential()
+    model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+    model.add(Dense(24, activation='relu'))
+    model.add(Dense(self.action_size, activation='linear'))
+    model.compile(loss=self._huber_loss,
+            optimizer=Adam(lr=self.learning_rate))
+    return model
 
-    def update_target_model(self):
-        # copy weights from model to target_model
-        self.target_model.set_weights(self.model.get_weights())
+  def update_target_model(self):
+    # copy weights from model to target_model
+    self.target_model.set_weights(self.model.get_weights())
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
+  def remember(self, state, action, reward, next_state, done):
+    self.memory.append((state, action, reward, next_state, done))
 
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+  def act(self, state):
+    if np.random.rand() <= self.epsilon:
+      return random.randrange(self.action_size)
+    act_values = self.model.predict(state)
+    return np.argmax(act_values[0])  # returns action
 
-    def replay(self, batch_size):
-        minibatch = random.sample(self.memory, batch_size)
-        # print('minibatch shape', minibatch[0])
-        for state, action, reward, next_state, done in minibatch:
-            # print('state shape', state.shape)
-            target = self.model.predict(state)
-            if done:
-                target[0][action] = reward
-            else:
-                a = self.model.predict(next_state)[0]
-                t = self.target_model.predict(next_state)[0]
-                target[0][action] = reward + self.gamma * t[np.argmax(a)]
-            self.model.fit(state, target, epochs=1, verbose=0)
-        if self.epsilon > self.epsilon_min:
-            # TODO linear decay?
-            self.epsilon *= self.epsilon_decay
+  def replay(self, batch_size):
+    minibatch = random.sample(self.memory, batch_size)
+    # print('minibatch shape', minibatch[0])
+    for state, action, reward, next_state, done in minibatch:
+      # print('state shape', state.shape)
+      target = self.model.predict(state)
+      if done:
+        target[0][action] = reward
+      else:
+        a = self.model.predict(next_state)[0]
+        t = self.target_model.predict(next_state)[0]
+        target[0][action] = reward + self.gamma * t[np.argmax(a)]
+      self.model.fit(state, target, epochs=1, verbose=0)
+    if self.epsilon > self.epsilon_min:
+      # TODO linear decay?
+      self.epsilon *= self.epsilon_decay
 
-    def load(self, name):
-        self.model.load_weights(name)
+  def load(self, name):
+    self.model.load_weights(name)
 
-    def save(self, name):
-        self.model.save_weights(name)
+  def save(self, name):
+    self.model.save_weights(name)
