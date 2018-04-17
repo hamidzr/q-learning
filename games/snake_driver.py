faf32d5c7edcd9a11b6df28c381fc2fc40ede50a
Hamid Zare <dellydela@gmail.com>
2018-04-16 19:30:50 -0500

    WIP added rewards and completed step fn (snake-driver)

--- games/snake_driver.py
+++ games/snake_driver.py
@@ -2,31 +2,63 @@ from models.game import Game
 from utils.helpers import GameStats
 import numpy as np
 
-class TicTacToe(Game):
+"""
+stat definitions:
+  score: number of fruits
+  rewards: described in code
+"""
+
+class SnakeDriver(Game):
   def __init__(self, base_game=None):
     self._game = base_game
     self.stats = GameStats()
+    self.cur_direction = 0
 
   def state(self):
     """ the state should show:
       1. where the walls and the rest of the snake is.
       2. where the fruits are
     """
-    # or the naive way: show everything as a big matrix
+    # OPTIMIZE or the naive way: show everything as a big matrix
     SNAKE_NUM, FRUIT_NUM = 0.5, -1
     state = np.zeros((self._game.board_size, self._game.board_size))
     for pt in self._game.snake:
       state[pt[0]][pt[1]] = SNAKE_NUM
-    for pt in self._game.fruit:
-      state[pt[0]][pt[1]] = FRUIT_NUM
+    fruit = self._game.fruit
+    state[fruit[0]][fruit[1]] = FRUIT_NUM
 
     return state
 
   def step(self, action):
-    pass
+    # 0: left 1: right
+    DIRS = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+    direction = self.cur_direction
+    if action == 0:
+      direction = (direction+3) % 4
+    elif action == 1:
+      direction = (direction+1) % 4
+    else:
+      assert action == 2, f'bad action {action}'
+
+    isAlive, gotFruit = self._game.step(DIRS[direction])
+    self.cur_direction = direction
+
+    reward = 0.1
+    if not isAlive:
+      reward -= 50
+    if gotFruit:
+      self.stats.add_score(1)
+      reward += 5
+
+    self.stats.add_reward(reward)
+
+    new_state = self.state()
+    isDone = not isAlive
+
+    return new_state, reward, isDone, None
 
   def show(self):
-    pass
+    self._game.print_field()
 
   def reset(self):
-    pass
+    self._game.reset()
