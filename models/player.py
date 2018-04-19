56222521c7226c505adc3c73704bd9f3647b548e
Hamid Zare <dellydela@gmail.com>
2018-04-18 22:39:49 -0500

    updated the player to support 1. playing against another AI 2. setting of role for games

--- models/player.py
+++ models/player.py
@@ -1,47 +1,73 @@
 import numpy as np
 import os.path
+from models.self_play import play
 
 # or trainee
 # game is the standardized game driver
 class Player:
-  def __init__(self, max_moves=None, name="aiPlayer", agent=None, game=None):
+  def __init__(self, max_moves=None, name="aiPlayer", agent=None, game=None, role=None):
     self.game = game
     self.agent = agent
     self.max_moves = max_moves #max number of moves in a game
     self.name = name
     self.save_loc = f'weights/{name}-player.h5'
+    self.last_action = None
+    self.last_state = None
+    self.role = role
+
+  def run_episode(self, resume, show, log, opponent):
+    self.game.reset()
+    if (opponent): # play against another AI
+      play(self, opponent)
+      self.on_episode_done()
+      opponent.on_episode_done()
+      self.log(log)
+      opponent.log(log)
+    else:
+      state = self.game.state()
+      for moveNum in range(self.max_moves):
+        # TODO factor out role 'X'
+        action = self.agent.act(state)
+        if self.role: # or handle it on step?
+          next_state, reward, isDone, info = self.game.step(action, self.role)
+        else:
+          next_state, reward, isDone, info = self.game.step(action)
+        self.agent.remember(state, action, reward, next_state, isDone)
+        state = next_state
+        if show: self.game.show()
+        if isDone:
+          print(f'finished episode w/ {moveNum} moves')
+          self.on_episode_done()
+          self.log(log)
+          break
+      ##### end of episode
+
+  def on_episode_done(self):
+    # reset episode related stat counters
+    self.game.stats.reset_episode()
+    self.agent.update_target_model()
+    # train the DNN if there are enough memories
+    self.agent.attempt_replay()
+    # put out logs TODO: based on win/loss or score
+
+  def log(self, system):
+    assert system, 'no logging system defined'
+    msg = f"e: {self.agent.epsilon:.2},"
+    if system == 'wins':
+      msg += f"win/all: {self.game.stats.win_rate()} - draws/all {self.game.stats.draw_rate()}"
+    elif system == 'score':
+      msg += f" score: {self.game.stats.score} avg:{self.game.stats.average_score()}, rewards: {float(self.game.stats.rewards):.5}"
+    print(msg)
+
 
   # log types 'wins' and 'score': (regression?!)
-  def train(self, episodes=10000, resume=False, save_freq=100, show=False, log='wins'):
+  def train(self, episodes=10000, resume=False, save_freq=100, show=False, log='wins', opponent=None):
     if resume and os.path.isfile(self.save_loc):
       self.agent.load(self.save_loc)
     for e in range(episodes):
-      self.game.reset()
-      state = self.game.state()
-      for moveNum in range(self.max_moves):
-        action = self.agent.act(state)
-        # TODO factor out role 'X'
-        next_state, reward, isDone, info = self.game.step(action)
-        self.agent.remember(state, action, reward, next_state, isDone)
-        state = next_state
-        if isDone: # when episode finished
-          # update game stats
-          self.agent.update_target_model()
-          # put out logs TODO: based on win/loss or score
-          msg = f"ep: {e}/{episodes}, moves: {moveNum}, e: {self.agent.epsilon:.2},"
-          if log == 'wins':
-            msg += f"win/all: {self.game.stats.win_rate()} - draws/all {self.game.stats.draw_rate()}"
-          elif log == 'score':
-            msg += f" score: {self.game.stats.score} avg:{self.game.stats.average_score()}, rewards: {float(self.game.stats.rewards):.5}"
-          print(msg)
-
-          if show: self.game.show()
-          # reset episode related stat counters
-          self.game.stats.reset_episode()
-          break
-        # end of episode
-      # train the DNN if there are enough memories
-      self.agent.attempt_replay()
+      print(f'ep: {e}/{episodes}')
+      self.run_episode(resume, show, log, opponent)
       # save an snapshot every so often
       if resume and e % save_freq == 0:
         self.agent.save(self.save_loc)
+        if opponent: opponent.agent.save(opponent.save_loc)
