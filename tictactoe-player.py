ded6d8f2b4338300c65dccdaaca24de0f6f2bbfe
Hamid Zare <dellydela@gmail.com>
2018-04-19 10:17:54 -0500

    cli args for tictactoe

--- tictactoe-player.py
+++ tictactoe-player.py
@@ -4,10 +4,16 @@ from games.tictactoe_driver import TicTacToe
 from games.tictactoe import Tic, get_enemy
 from utils.helpers import GameStats
 from models.player import Player
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--show", type=bool, default=False, help="show game progress")
+parser.add_argument("--save_resume", type=bool, default=False, help="save and resume?")
+args = parser.parse_args()
+
 
 EPISODES = 20000
 MAX_MOVES = 100 # maximum number of moves in a game
-SAVE_LOC = "weights/tictactoe-ddqn.h5"
 
 # create the game
 board = Tic(random_ratio=0.4)
@@ -16,11 +22,9 @@ game = TicTacToe(base_game=board)
 # setup the agent
 state_size = 9
 action_size = state_size
-agent = DQNAgent(state_size, action_size, epsilon=0.05,
+agent = DQNAgent(state_size, action_size, epsilon=1,
                  epsilon_decay=0.995, epsilon_min=0.01, batch_size=32)
-# agent.load(SAVE_LOC)
 
 aiPlayer = Player(game=game, max_moves=MAX_MOVES, name='tic-qlearner', agent=agent, role='X')
 
-# TODO factor out role from player
-aiPlayer.train(episodes=EPISODES, resume=False, show=False)
+aiPlayer.train(episodes=EPISODES, resume=args.save_resume, show=args.show)
