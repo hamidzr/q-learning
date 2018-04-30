aa1334c48530b444a6c45e452fcf53468c6b7730
Hamid Zare <dellydela@gmail.com>
2018-04-30 13:34:04 -0500

    indentation 4sp to 2sp

--- utils/helpers.py
+++ utils/helpers.py
@@ -152,31 +152,31 @@ class GameStats:
 
 
 class Link(object):
-    def __init__(self, value=0.0):
-        self.next = None
-        self.value = value
+  def __init__(self, value=0.0):
+    self.next = None
+    self.value = value
 
 class LinkedRing(object):
-    def __init__(self, length):
-        self.sum = 0.0
-        self.length = length
-        self.current = Link()
+  def __init__(self, length):
+    self.sum = 0.0
+    self.length = length
+    self.current = Link()
 
-        # Initialize all the nodes:
-        last = self.current
-        for i in range(length-1):  # one link is already created
-            last.next = Link()
-            last = last.next
-        last.next = self.current  # close the ring
+    # Initialize all the nodes:
+    last = self.current
+    for i in range(length-1):  # one link is already created
+      last.next = Link()
+      last = last.next
+    last.next = self.current  # close the ring
 
-    def add_val(self, val):
-        self.sum -= self.current.value
-        self.sum += val
-        self.current.value = val
-        self.current = self.current.next
+  def add_val(self, val):
+    self.sum -= self.current.value
+    self.sum += val
+    self.current.value = val
+    self.current = self.current.next
 
-    def average(self):
-        return self.sum / self.length
+  def average(self):
+    return self.sum / self.length
 
 
 def dump(fname, data):
